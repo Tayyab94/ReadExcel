10142af56b13625973665ecb8ba191a29736cd93
tayyab bhatti <tayyab.bhatti30@hotmail.com>
2020-04-01 02:38:08 +0500

    First  Commit

---
+++ ReadExcel/Program.cs
@@ -0,0 +1,79 @@
+﻿using ExcelDataReader;
+using OfficeOpenXml;
+using System;
+using System.Data;
+using System.IO;
+using System.Linq;
+
+namespace ReadExcel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //var dtContent=GetDataTableFromExcel(@"C:\Users\tayya\Desktop\BackLinks File\web-2.0documentarticleforum.xlsx");
+
+
+            //foreach (DataRow dr in dtContent.Rows)
+            //{
+            //    Console.WriteLine(dr["Name"].ToString());
+            //}
+
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            using (var stream = File.Open(@"C:\Users\Admin\Desktop\BackLinks File\web-2.0documentarticleforum.xlsx", FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    do
+                    {
+                        while (reader.Read()) //Each ROW
+                        {
+                            for (int column = 0; column < reader.FieldCount; column++)
+                            {
+                                //Console.WriteLine(reader.GetString(column));//Will blow up if the value is decimal etc. 
+                                Console.WriteLine(reader.GetValue(column));//Get Value returns object
+                            }
+                        }
+                    } while (reader.NextResult()); //Move to NEXT SHEET
+
+                }
+            }
+
+            Console.WriteLine("End Ho gya!");
+
+            Console.ReadLine();
+        }
+
+
+        private static DataTable GetDataTableFromExcel(string path, bool hasHeader = true)
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            using (var pck = new OfficeOpenXml.ExcelPackage())
+            {
+                using (var stream = File.OpenRead(path))
+                {
+                    pck.Load(stream);
+                }
+                var ws = pck.Workbook.Worksheets.First();
+                DataTable tbl = new DataTable();
+                foreach (var firstRowCell in ws.Cells[1, 1, 1, ws.Dimension.End.Column])
+                {
+                    tbl.Columns.Add(hasHeader ? firstRowCell.Text : string.Format("Column {0}", firstRowCell.Start.Column));
+                }
+                var startRow = hasHeader ? 2 : 1;
+                for (int rowNum = startRow; rowNum <= ws.Dimension.End.Row; rowNum++)
+                {
+                    var wsRow = ws.Cells[rowNum, 1, rowNum, ws.Dimension.End.Column];
+                    DataRow row = tbl.Rows.Add();
+                    foreach (var cell in wsRow)
+                    {
+                        row[cell.Start.Column-1] = cell.Text;
+                    }
+                }
+                return tbl;
+            }
+        }
+    }
+}
